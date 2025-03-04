9ecb72acf43f09f868c3023b828365800d95b313
thesunnatillo <sunnatillocoder@gmail.com>
2025-03-04 07:46:29 +0500

    feat: migrations

---
+++ src/database/entities/base.entity.ts
@@ -0,0 +1,23 @@
+import {
+    BaseEntity,
+    CreateDateColumn,
+    DeleteDateColumn,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn
+} from "typeorm";
+
+export abstract class GeneralEntity extends BaseEntity {
+
+    @PrimaryGeneratedColumn()
+        id: number;
+
+    @CreateDateColumn({ name: 'created_at' })
+        createdAt: Date;
+
+    @UpdateDateColumn({ name: 'updated_at' })
+        updatedAt: Date;
+
+    @DeleteDateColumn({ name: 'deleted_at' })
+        deletedAt: Date;
+
+}
