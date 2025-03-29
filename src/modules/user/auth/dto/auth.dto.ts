25df1e103965fbc93743e8640b916353b30540f3
thesunnatillo <sunnatillocoder@gmail.com>
2025-03-29 05:39:02 +0500

    ref: added swagger property

--- src/modules/user/auth/dto/auth.dto.ts
+++ src/modules/user/auth/dto/auth.dto.ts
@@ -1,26 +1,36 @@
 import { MaxLength } from '@app/validator/max-length.validator';
 import { MinLength } from '@app/validator/min-length.validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class SignUpDto {
-  @MinLength(10)
+
+  @ApiProperty()
+  @MinLength(5)
   @MaxLength(55)
   fullName: string;
 
+  @ApiProperty()
   @MinLength(5)
   @MaxLength(32)
   login: string;
 
+  @ApiProperty()
   @MinLength(8)
   @MaxLength(24)
   password: string;
+
 }
 
 export class SignInDto {
+
+  @ApiProperty()
   @MinLength(5)
   @MaxLength(32)
   login: string;
 
+  @ApiProperty()
   @MinLength(8)
   @MaxLength(24)
   password: string;
+
 }
