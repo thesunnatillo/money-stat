ef24292b0e645cc2af2ea8438044c464cabc6519
thesunnatillo <sunnatillocoderl@gmail.com>
2025-04-25 17:02:32 +0500

    feat: add decorator

---
+++ src/decorator/payload.decorator.ts
@@ -0,0 +1,12 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+
+export const Payload = createParamDecorator(
+
+    (data: string, ctx: ExecutionContext) => {
+
+        const request = ctx.switchToHttp().getRequest();
+
+        return request.payload;
+
+    }
+);
